571cc7e3cca9400c0622b3c34370521407e00bff
Francisco Luque <fluque1995@gmail.com>
2018-01-14 21:15:12 +0100

    Fixed plots

--- report/csv/generate_plot.py
+++ report/csv/generate_plot.py
@@ -2,14 +2,14 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filename = "accuracy_dropout.csv"
+filename = "accuracy_vlr_1.csv"
 
 data = pd.read_csv(filename)
 
 sns.set()
 fig = sns.pointplot(x="Step",
                     y="Value",
-                    data=data)
+                    data=data[::4])
 
 plt.xticks(rotation=60)
 plt.show()
